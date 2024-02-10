03c08d5c5c5e0bf133fd5d50d663675538a4a359
NesliUsta <147261188+NesliUsta@users.noreply.github.com>
2024-02-10 12:30:43 -0500

    Create testNeslihan.java

---
+++ src/utils/testNeslihan.java
@@ -0,0 +1,17 @@
+package utils;
+
+public class DataBaseUtil {
+
+    public static void connection(){
+
+    }
+
+    public static void readRow(){
+
+    }
+
+    public static void readColm(){
+
+    }
+
+}
